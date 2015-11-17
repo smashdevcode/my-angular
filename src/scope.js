40fa9df783fa591f752492adf788fcc7a32f3112
James Churchill <james@smashdev.com>
2015-11-17 09:49:43 -0800

    Further work on the Scope object

--- src/scope.js
+++ src/scope.js
@@ -9,12 +9,14 @@ function initWatchVal() {
 function Scope() {
     this.$$watchers = [];
     this.$$lastDirtyWatch = null;
+    this.$$asyncQueue = [];
 }
 
-Scope.prototype.$watch = function (watchFn, listenerFn) {
+Scope.prototype.$watch = function (watchFn, listenerFn, valueEq) {
     var watcher = {
         watchFn: watchFn,
         listenerFn: listenerFn || function() { },
+        valueEq: !!valueEq,
         last: initWatchVal
     };
     this.$$watchers.push(watcher);
@@ -27,9 +29,11 @@ Scope.prototype.$$digestOnce = function () {
     _.forEach(this.$$watchers, function (watcher) {
         newValue = watcher.watchFn(self);
         oldValue = watcher.last;
-        if (newValue !== oldValue) {
+        if (!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
             self.$$lastDirtyWatch = watcher;
-            watcher.last = newValue;
+            // If we are comparing by value then we need to do a deep copy
+            // of the new value when we are updating the "last" property
+            watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
             watcher.listenerFn(newValue,
                 (oldValue === initWatchVal ? newValue : oldValue),
                 self);
@@ -45,9 +49,39 @@ Scope.prototype.$digest = function () {
     var ttl = 10, dirty;
     this.$$lastDirtyWatch = null;
     do {
+        while (this.$$asyncQueue.length) {
+            var asyncTask = this.$$asyncQueue.shift();
+            asyncTask.scope.$eval(asyncTask.expression);
+        }
         dirty = this.$$digestOnce();
         if (dirty && !(ttl--)) {
             throw '10 digest iterations reached';
         }
     } while (dirty);
+};
+
+Scope.prototype.$$areEqual = function (newValue, oldValue, valueEq) {
+    if (valueEq) {
+        return _.isEqual(newValue, oldValue);
+    } else {
+        return newValue === oldValue ||
+            (typeof newValue === 'number' && typeof oldValue === 'number' &&
+            isNaN(newValue) && isNaN(oldValue));
+    }
+};
+
+Scope.prototype.$eval = function (expr, locals) {
+    return expr(this, locals);
+};
+
+Scope.prototype.$apply = function (expr) {
+    try {
+        return this.$eval(expr);
+    } finally {
+        this.$digest();
+    }
+};
+
+Scope.prototype.$evalAsync = function (expr) {
+    this.$$asyncQueue.push({ scope: this, expression: expr });
 };
